6ad6b128d1a6e5407faf362b3b0112801549dd4f
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-03-31 04:45:12 +0200

    Notification page
    Adding notification component to display notifications.

--- src/notificationPage.jsx
+++ src/notificationPage.jsx
@@ -1,3 +1,17 @@
 export const NotificationPage = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div className="notification-page">
+      <div className="card-header">
+        <div className="badge">
+          <h2>Notifications</h2>
+
+          <div className="badge-notification">3</div>
+        </div>
+
+        <div className="read">
+          <a href="!#">Mark all as read</a>
+        </div>
+      </div>
+    </div>
+  );
 };
