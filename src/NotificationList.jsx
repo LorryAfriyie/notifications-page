97ac3471ef8254fc129dd8088b51e7c152ec5ae1
Lawrence Afriyie <lawrenceafriyie35@gmail.com>
2024-04-04 03:14:36 +0200

    Padding and box shadow adjustment

--- src/NotificationList.jsx
+++ src/NotificationList.jsx
@@ -110,3 +110,7 @@ const privateMessage = [
       "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots  of fun and improving my game.",
   },
 ];
+
+const imageComment = [
+  { imgCommentId: 1, personId: 5, img: "/images/image-chess.webp" },
+];
